f400ea1855bc0737546e24ee64d33819772f87ed
Sofia Grosu <sofia.grosu@stud.ubbcluj.ro>
2025-03-06 19:56:22 +0200

    seminar1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,27 @@
+#include <iostream>
+class Dog {
+public:
+  bool eat() {
+    std::cout<<"Dog is eating"<<std::endl;
+    return true;
+  }
+  int get_age() {
+    return age;
+  }
+  void set_age(int a) {
+    age=a;
+  }
+private:
+    int age;
+
+};
 int main(){
+  Dog dog1=Dog();
+  dog1.eat();
+  dog1.set_age(10);
+  std::cout<<dog1.get_age();
+
   return 0;
   }
+
+
